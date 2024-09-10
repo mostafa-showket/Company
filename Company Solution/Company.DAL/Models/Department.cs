e23e2486b5986d381610f1446d8000e72724338b
Mostafa Khaled Showket <114008599+mostafa-showket@users.noreply.github.com>
2024-09-10 19:40:05 +0300

    added code to view in index of Departments
    added some validation to department model

--- Company Solution/Company.DAL/Models/Department.cs
+++ Company Solution/Company.DAL/Models/Department.cs
@@ -1,10 +1,19 @@
-﻿namespace Company.DAL.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Company.DAL.Models
 {
     public class Department
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Code is Required!")]
         public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name is Required!")]
         public string Name { get; set; }
+
+        [DisplayName("Date Of Creation")]
         public DateTime DateOfCreation { get; set; }
     }
 }
