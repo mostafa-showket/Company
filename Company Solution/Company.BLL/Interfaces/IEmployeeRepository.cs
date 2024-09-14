c38d67b7ceb749e16752456a4ca7d71acfe3bdb9
Mostafa Khaled Showket <114008599+mostafa-showket@users.noreply.github.com>
2024-09-14 15:50:56 +0300

    Added Employee Repository and its interface

---
+++ Company Solution/Company.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Company.DAL.Models;
+
+namespace Company.BLL.Interfaces
+{
+    internal interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee Get(int id);
+        int Add(Employee entity);
+        int Update(Employee entity);
+        int Delete(Employee entity);
+    }
+}
---
+++ Company Solution/Company.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,40 @@
+﻿using Company.BLL.Interfaces;
+using Company.DAL.Data.Contexts;
+using Company.DAL.Models;
+
+namespace Company.BLL.Repositories
+{
+    internal class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employee> GetAll() => _context.Employees.ToList();
+
+
+        public Employee Get(int id) => _context.Employees.Find(id);
+
+
+        public int Add(Employee entity)
+        {
+            _context.Employees.Add(entity);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Employee entity)
+        {
+            _context.Employees.Update(entity);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Employee entity)
+        {
+            _context.Employees.Remove(entity);
+            return _context.SaveChanges();
+        }
+    }
+}
