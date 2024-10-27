642b6b1f3e57a076292b5e2ea959aaa8c6a52736
Mostafa Khaled Showket <114008599+mostafa-showket@users.noreply.github.com>
2024-10-28 00:46:25 +0300

    added Auth layout and signup form with styles

---
+++ Company Solution/Company.PL/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        // SignUp
+
+        [HttpGet] // /Account/SignUp
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Company Solution/Company.PL/ViewModels/Auth/SignUpViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.PL.ViewModels.Auth
+{
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage = "UserName is Required !")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "First name is Required !")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "last name is Required !")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is Required !")]
+        [EmailAddress(ErrorMessage = "Invalid Email !")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is Required !")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "ConfirmPassword is Required !")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Confirmed Password doesn't match Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "IsAgree is Required !")]
+        public bool IsAgree { get; set; }
+    }
+}
